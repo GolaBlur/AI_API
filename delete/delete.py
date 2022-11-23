252320213b032013c0157bf1dd7ab31984a87719
dackyy <dack1yy@gmail.com>
2022-11-23 20:52:54 +0900

    update

--- delete/delete.py
+++ delete/delete.py
@@ -1,4 +1,7 @@
-
+import sys
+sys.path.append("C:/Users/eorl6/Documents/golablur")
+import golablur
 class ex_delete:
-    def ex():
+    def img():
+        img = golablur.Image(381,158,381,29,'C:/Users/eorl6/Documents/golablur/car.jpg')
         return
